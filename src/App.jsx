47fb98a4a3c6c5d2071f9f1d14642537c54a2acb
cpicazo8304 <cpicazo8304@gmail.com>
2024-10-17 21:04:17 -0500

    added data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css'
 
 function App() {
   const [dates, setDates] = useState(null);
-  const [data, setDate] = useState('');
+  const [date, setDate] = useState('');
   const [maxTemperatures, setMaxTemperatures] = useState(null);
   const [minTemperatures, setMinTemperatures] = useState(null);
   const [max, setMax] = useState(0);
   const [min, setMin] = useState(0);
   const [numDays, setNumDays] = useState(0);
+  const [ranges, setRanges] = useState([]);
+  const [isFiltered, setFiltered] = useState(false);
+  const [filteredList, setFilteredResults] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchAPIData = async() => {
@@ -18,9 +22,15 @@ function App() {
       setDates(data.daily.time);
       setMaxTemperatures(data.daily.temperature_2m_max);
       setMinTemperatures(data.daily.temperature_2m_min);
-      
+
       setMax(Math.max(...maxTemperatures));
       setMin(Math.min(...minTemperatures));
+
+      const rangeList = maxTemperatures.map((item, index) => (
+        Math.round(item - minTemperatures[index])
+      ));
+
+      setRanges(rangeList);
       setNumDays(data.daily.time.length);
     };
     fetchAPIData();
@@ -55,17 +65,28 @@ function App() {
         </div>
 
         <div className='dashboard'>
-          <input type='text' placeHolder='Enter Date' onChange={(e) => {setDate(e.target.value)}}/>
-          <table>
+          <div className='filter'>
+            <input type='text' placeHolder='Enter Date' onChange={(e) => {setDate(e.target.value)}} className='search'/>
+           
+          </div>
+          <table className='data'>
             <thead>
               <tr>
                 <th>Date</th>
                 <th>Max Temperature (°F)</th>
                 <th>Min Temperature (°F)</th>
+                <th>Temperature Range (°F)</th>
               </tr>
             </thead>
             <tbody>
-          
+              {dates.map((item, index) => (
+                <tr>
+                  <td>{item}</td>
+                  <td>{maxTemperatures[index]}</td>
+                  <td>{minTemperatures[index]}</td>
+                  <td>{ranges[index]}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
